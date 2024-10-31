84ab4b62cc4408399b5996bfaf71ee78d4094193
leoneville <leoneville_@hotmail.com>
2024-10-31 12:23:04 -0300

    feat: Implementing pets_repository with first test

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,20 @@
+import pytest
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+from src.models.sqlite.settings.connection import db_connection_handler
+
+# db_connection_handler.connect_to_db()
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_list_pets():
+    repo = PetsRepository(db_connection_handler)
+    response = repo.list_pets()
+    print(response)
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_delete_pet():
+    name = "burro"
+
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
