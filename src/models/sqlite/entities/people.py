1e9033eebd238c61fba8abbafbdec9a0db1d2806
leoneville <leoneville_@hotmail.com>
2024-10-31 18:18:45 -0300

    feat: Implementing people_repository

--- src/models/sqlite/entities/people.py
+++ src/models/sqlite/entities/people.py
@@ -1,15 +1,17 @@
+from dataclasses import dataclass
 from sqlalchemy import Column, String, BIGINT, ForeignKey
 from src.models.sqlite.settings.base import Base
 
 
+@dataclass
 class PeopleTable(Base):
     __tablename__ = "people"
 
-    id = Column(BIGINT, primary_key=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    age = Column(BIGINT, nullable=False)
-    pet_id = Column(BIGINT, ForeignKey("pets.id"))
+    id: int = Column(BIGINT, primary_key=True)
+    first_name: str = Column(String, nullable=False)
+    last_name: str = Column(String, nullable=False)
+    age: int = Column(BIGINT, nullable=False)
+    pet_id: int = Column(BIGINT, ForeignKey("pets.id"))
 
     def __repr__(self) -> str:
         return f"People [name={self.first_name}, last_name={self.last_name}, pet_id={self.pet_id}]"
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,4 +1,5 @@
 import pytest
+from src.models.sqlite.repositories.people_repository import PeopleRepository
 from src.models.sqlite.repositories.pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
 
@@ -18,3 +19,26 @@ def test_delete_pet():
 
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_insert_person():
+    first_name = "test name"
+    last_name = "test last"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(
+        first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
+    )
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print(response)
+    print(response.pet_name)
