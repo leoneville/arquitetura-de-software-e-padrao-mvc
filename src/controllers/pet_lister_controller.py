46bac854c3ce9ff6aaf83ab923e140f568de7c70
leoneville <leoneville_@hotmail.com>
2024-11-03 09:59:37 -0300

    feat: Implementing pet_lister_controller with tests

---
+++ src/controllers/pet_lister_controller.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+from src.models.sqlite.entities.pets import PetsTable
+from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
+
+
+class PetListerController:
+    def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
+        self.__pet_repository = pet_repository
+
+    def list(self) -> Dict:
+        pets = self.__get_pets_in_db()
+        response = self.__format_response(pets)
+        return response
+
+    def __get_pets_in_db(self) -> List[PetsTable]:
+        pets = self.__pet_repository.list_pets()
+        return pets
+
+    def __format_response(self, pets: List[PetsTable]) -> Dict:
+        formatted_pets = [
+            {"name": pet.name, "type": pet.type, "id": pet.id} for pet in pets
+        ]
+
+        return {
+            "data": {
+                "type": "Pets",
+                "count": len(formatted_pets),
+                "attributes": formatted_pets,
+            }
+        }
---
+++ src/controllers/pet_lister_controller_test.py
@@ -0,0 +1,28 @@
+from src.controllers.pet_lister_controller import PetListerController
+from src.models.sqlite.entities.pets import PetsTable
+
+
+class MockPetsRepository:
+    def list_pets(self):
+        return [
+            PetsTable(name="Fluffy", type="Cat", id=4),
+            PetsTable(name="Buddy", type="Dog", id=47),
+        ]
+
+
+def test_list_pets():
+    controller = PetListerController(MockPetsRepository())
+    response = controller.list()
+
+    expected_response = {
+        "data": {
+            "type": "Pets",
+            "count": 2,
+            "attributes": [
+                {"name": "Fluffy", "type": "Cat", "id": 4},
+                {"name": "Buddy", "type": "Dog", "id": 47},
+            ],
+        }
+    }
+
+    assert response == expected_response
