2aec95fcabf559738aa2eb8709333d4d5d2d9c36
leoneville <leoneville_@hotmail.com>
2024-11-04 22:03:23 -0300

    feat: Implementing person_create_view (need tests)

--- src/controllers/interfaces/person_creator_controller.py
+++ src/controllers/interfaces/person_creator_controller.py
@@ -2,7 +2,7 @@ from typing import Dict
 from abc import ABC, abstractmethod
 
 
-class PersonCreateControllerInterface(ABC):
+class PersonCreatorControllerInterface(ABC):
     @abstractmethod
     def create(self, person_info: Dict) -> Dict:
         pass
--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,12 +1,12 @@
 import re
 from typing import Dict
 from src.controllers.interfaces.person_creator_controller import (
-    PersonCreateControllerInterface,
+    PersonCreatorControllerInterface,
 )
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
 
 
-class PersonCreatorController(PersonCreateControllerInterface):
+class PersonCreatorController(PersonCreatorControllerInterface):
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
 
